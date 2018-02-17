e177946d59c404bb1d77a569197da41c197b4d5d
Areax <actuallythisisspam@gmail.com>
2018-02-17 17:54:49 -0500

    WIP for PotStove minigame.

---
+++ Assets/Scripts/Meter.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Meter : MonoBehaviour
+{
+
+    public float progress;
+    public Vector2 pos;
+    public Vector2 size;
+    public Texture2D emptyTex;
+    public Texture2D fullTex;
+    public Transform knobProgress;
+    private IEnumerator coroutine;
+    float randomValue;
+    float incValue;
+    float timer;
+
+    void OnGUI()
+    {
+
+        float posX = Screen.width * pos.x;
+        float posY = Screen.height * pos.y;
+
+        //draw the background:
+        GUI.BeginGroup(new Rect(posX, posY, size.x, size.y));
+        GUI.DrawTexture(new Rect(0, 0, size.x, size.y), emptyTex);
+
+        //draw the filled-in part:
+        //GUI.BeginGroup(new Rect(0, 0, size.x * progress, size.y));
+        //GUI.DrawTexture(new Rect(0,0, size.x, size.y), fullTex);
+        //draw the filled-in part FLIPPED:
+        int xProg = (int)(size.x * progress);
+        GUI.BeginGroup(new Rect(size.x - xProg, 0, xProg, size.y));
+        GUI.DrawTexture(new Rect(-size.x + xProg, 0, size.x, size.y), fullTex);
+
+        GUI.EndGroup();
+        GUI.EndGroup();
+    }
+
+    IEnumerator startValue()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timer);
+            randomValue = randomRange(falseSigmoid(progress));
+            incValue += randomValue / 100f;
+            timer = 4f + randomRange(1.5f);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        progress = 0.75f;
+        timer = 8f;
+        pos = new Vector2(0.67f, 0.02f);
+        size = new Vector2(256, 256);
+        coroutine = startValue();
+        StartCoroutine(coroutine);
+
+        randomValue = 0f;
+        incValue = 0f;
+    }
+
+    // The larger the number, the greater the variance
+    float randomRange(float p)
+    {
+        return Random.Range(p, -p);
+    }
+
+    float falseSigmoid(float value)
+    {
+        return (float)(value / (1 + Mathf.Abs(value)));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float p = (knobProgress.localEulerAngles.z / 360.0f);
+        if(incValue != randomValue)
+        {
+            if (incValue < randomValue)
+                incValue += .001f;
+            else incValue -= .001f;
+        }
+
+        progress = (knobProgress.localEulerAngles.z / 360.0f) < .05 || (knobProgress.localEulerAngles.z / 360.0f) > .95 ? 0 : 1 - p + incValue + randomRange(falseSigmoid(.005f));
+
+    }
+}
---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+
+    Vector3 mouse_pos;
+    Vector3 object_pos;
+    float angle;
+    public bool isLocal;
+    bool isClicked = false;
+
+	// Use this for initialization
+	void Start() {
+
+    }
+
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 5f;
+
+            Vector2 v = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            Collider2D[] col = Physics2D.OverlapPointAll(v);
+
+            if (col.Length > 0)
+            {
+                foreach (Collider2D c in col)
+                {
+                    if (this.gameObject == c.gameObject)
+                    {
+                        isClicked = true;
+                    }
+                }
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if(isClicked)
+            {
+                mouse_pos = Input.mousePosition;
+                mouse_pos.z = -20;
+                object_pos = Camera.main.WorldToScreenPoint(this.transform.position);
+                mouse_pos.x = mouse_pos.x - object_pos.x;
+                mouse_pos.y = mouse_pos.y - object_pos.y;
+                angle = (Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg) - 90;
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            }
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            isClicked = false;
+        }
+    }
+}
+ 
