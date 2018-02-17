139039cdefb8ce9fe8f7c7d80db9680e3aac1287
tclark61 <tclark61@knights.ucf.edu>
2018-02-17 12:29:05 -0500

    Added some knife scripts

---
+++ Assets/Scripts/ShiftObjectOnClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShiftObjectOnClick : MonoBehaviour {
+
+    public int lowerPosition = 8;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - lowerPosition);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + lowerPosition);
+        }
+	}
+}
