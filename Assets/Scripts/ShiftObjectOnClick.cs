7291526a723466e4ac252df0d79080ee93f0dadb
tclark61 <tclark61@knights.ucf.edu>
2018-02-17 14:17:33 -0500

    test

--- Assets/Scripts/ShiftObjectOnClick.cs
+++ Assets/Scripts/ShiftObjectOnClick.cs
@@ -2,24 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShiftObjectOnClick : MonoBehaviour {
+public class ShiftObjectOnClick : MonoBehaviour
+{
 
-    public int lowerPosition = 8;
+    public int lowerPosition = 1;
+    GameObject knife;
+    // Use this for initialization
+    void Start()
+    {
+        knife = GameObject.Find("Cube");
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y - lowerPosition);
+            knife.gameObject.transform.localScale += new Vector3(0, -1, 0);
+            transform.position = new Vector3(transform.position.x, transform.position.y + lowerPosition);
         }
         if (Input.GetMouseButtonUp(0))
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y + lowerPosition);
+            transform.position = new Vector3(transform.position.x, transform.position.y - lowerPosition);
+            knife.gameObject.transform.localScale += new Vector3(0, 1, 0);
         }
-	}
+    }
+
 }
