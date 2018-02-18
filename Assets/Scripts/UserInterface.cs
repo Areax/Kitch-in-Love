81e7902594eb885552e5fb413eb1770d92b9aa2c
tclark61 <tclark61@knights.ucf.edu>
2018-02-18 16:04:47 -0500

    Check these sweet changes

---
+++ Assets/Scripts/UserInterface.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UserInterface : MonoBehaviour
+{
+    public GameObject rr;
+    public GameObject gameOver;
+
+    bool escMenu = false;
+    List<GameObject> objArray;
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 1;
+        objArray = new List<GameObject>();
+        rr.SetActive(false);
+        Debug.Log(rr);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            EscapePressed();
+        }
+    }
+
+
+    void EscapePressed()
+    {
+        //Pause or unpause the game
+        if (Time.timeScale == 0)
+        {
+            Resume();
+        }
+        else
+        {
+            Time.timeScale = 0;
+            escMenu = true;
+            rr.SetActive(escMenu);
+
+        }
+    }
+
+    public void Resume()
+    {
+        escMenu = false;
+        rr.SetActive(escMenu);
+        Time.timeScale = 1;
+    }
+
+    public void playAgain()
+    {
+        gameOver.SetActive(false);
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GoToMain()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Main Menu");
+    }
+
+}
